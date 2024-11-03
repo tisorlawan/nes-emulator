28fdb059fb84f50de3542fad5b916097854d0726
Agung Baptiso Sorlawan <tisorlawan@gmail.com>
2024-11-03 21:39:44 +0700

    feat: improve status feature

--- src/cpu.zig
+++ src/cpu.zig
@@ -42,6 +42,17 @@ pub const AddrMode = enum {
     None,
 };
 
+pub const Status = enum(u8) {
+    Carry = 0b0000_0001,
+    Zero = 0b0000_0010,
+    InterruptDisable = 0b0000_0100,
+    DecimalMode = 0b0000_1000,
+    Break = 0b0001_0000,
+    Break2 = 0b0010_0000,
+    Overflow = 0b0100_0000,
+    Negative = 0b1000_0000,
+};
+
 pub const OpCode = struct {
     code: u8, // Opcode
     mnemonic: []const u8, // instruction name
@@ -50,6 +61,18 @@ pub const OpCode = struct {
     mode: AddrMode, // address mode
     plus_if_crossed: u8, // + if crossed
 };
+
+pub const BRK = 0x00;
+pub const NOP = 0xEA;
+
+pub const ADC_IMM = 0x69;
+pub const ADC_ZP = 0x65;
+pub const ADC_ZPX = 0x75;
+pub const ADC_ABS = 0x6D;
+pub const ADC_ABSX = 0x7D;
+pub const ADC_ABSY = 0x79;
+pub const ADC_INDX = 0x61;
+pub const ADC_INDY = 0x71;
 
 pub const LDA_IMM = 0xA9;
 pub const LDA_ZP = 0xA5;
@@ -61,7 +84,6 @@ pub const LDA_INDX = 0xA1;
 pub const LDA_INDY = 0xB1;
 
 // OpCode constants for BRK, TAX, INX
-pub const BRK = 0x00;
 pub const TAX = 0xAA;
 pub const INX = 0xE8;
 
@@ -75,6 +97,18 @@ pub const STA_INDX = 0x81;
 pub const STA_INDY = 0x91;
 
 const opCodes = [_]OpCode{
+    .{ .code = BRK, .mnemonic = "BRK", .len = 1, .cycles = 7, .mode = .None, .plus_if_crossed = 0 },
+    .{ .code = NOP, .mnemonic = "NOP", .len = 1, .cycles = 2, .mode = .None, .plus_if_crossed = 0 },
+
+    .{ .code = ADC_IMM, .mnemonic = "ADC", .len = 2, .cycles = 2, .mode = .Immediate, .plus_if_crossed = 0 },
+    .{ .code = ADC_ZP, .mnemonic = "ADC", .len = 2, .cycles = 3, .mode = .ZeroPage, .plus_if_crossed = 0 },
+    .{ .code = ADC_ZPX, .mnemonic = "ADC", .len = 2, .cycles = 4, .mode = .ZeroPageX, .plus_if_crossed = 0 },
+    .{ .code = ADC_ABS, .mnemonic = "ADC", .len = 3, .cycles = 4, .mode = .Absolute, .plus_if_crossed = 0 },
+    .{ .code = ADC_ABSX, .mnemonic = "ADC", .len = 3, .cycles = 4, .mode = .AbsoluteX, .plus_if_crossed = 1 },
+    .{ .code = ADC_ABSY, .mnemonic = "ADC", .len = 3, .cycles = 4, .mode = .AbsoluteY, .plus_if_crossed = 1 },
+    .{ .code = ADC_INDX, .mnemonic = "ADC", .len = 2, .cycles = 6, .mode = .IndirectX, .plus_if_crossed = 0 },
+    .{ .code = ADC_INDY, .mnemonic = "ADC", .len = 2, .cycles = 5, .mode = .IndirectY, .plus_if_crossed = 1 },
+
     // Load Accumulator (LDA) instructions
     .{ .code = LDA_IMM, .mnemonic = "LDA", .len = 2, .cycles = 2, .mode = .Immediate, .plus_if_crossed = 0 },
     .{ .code = LDA_ZP, .mnemonic = "LDA", .len = 2, .cycles = 3, .mode = .ZeroPage, .plus_if_crossed = 0 },
@@ -85,8 +119,7 @@ const opCodes = [_]OpCode{
     .{ .code = LDA_INDX, .mnemonic = "LDA", .len = 2, .cycles = 6, .mode = .IndirectX, .plus_if_crossed = 0 },
     .{ .code = LDA_INDY, .mnemonic = "LDA", .len = 2, .cycles = 5, .mode = .IndirectY, .plus_if_crossed = 1 },
 
-    // Break, Transfer A to X, and Increment X instructions
-    .{ .code = BRK, .mnemonic = "BRK", .len = 1, .cycles = 7, .mode = .None, .plus_if_crossed = 0 },
+    // Transfer A to X, and Increment X instructions
     .{ .code = TAX, .mnemonic = "TAX", .len = 1, .cycles = 2, .mode = .None, .plus_if_crossed = 0 },
     .{ .code = INX, .mnemonic = "INX", .len = 1, .cycles = 2, .mode = .None, .plus_if_crossed = 0 },
 
@@ -154,7 +187,7 @@ const CPU = struct {
     }
 
     pub fn load(self: *CPU, program: []const u8) void {
-        std.mem.copyForwards(u8, self.memory[PROG_ROM_START_ADDR .. PROG_ROM_START_ADDR + program.len], program);
+        std.mem.copyForwards(u8, self.memory[PROG_ROM_START_ADDR..][0..program.len], program);
         self.pc = PROG_ROM_START_ADDR;
         self.memWriteU16(ADDR_START, PROG_ROM_START_ADDR);
     }
@@ -163,6 +196,18 @@ const CPU = struct {
         self.load(program);
         self.reset();
         self.run();
+    }
+
+    pub fn statusSet(self: *CPU, status: Status) void {
+        self.status |= @intFromEnum(status);
+    }
+
+    pub fn statusUnset(self: *CPU, status: Status) void {
+        self.status &= (~@intFromEnum(status));
+    }
+
+    pub fn statusIs(self: *CPU, status: Status) bool {
+        return (self.status & @intFromEnum(status)) != 0;
     }
 
     pub fn run(self: *CPU) void {
@@ -175,6 +220,10 @@ const CPU = struct {
             // std.debug.print("OP = {s} = {any}\n", .{ OpcodeMap.get(opcode).?.mnemonic, OpcodeMap.get(opcode).? });
             switch (opcode) {
                 BRK => break,
+                NOP => {},
+                // ADC_IMM, ADC_ZP, ADC_ZPX, ADC_ABS, ADC_ABSX, ADC_ABSY, ADC_INDX, ADC_INDY => {
+                //     self.adc(OpcodeMap.get(opcode).?.mode);
+                // },
                 LDA_IMM, LDA_ZP, LDA_ZPX, LDA_ABS, LDA_ABSX, LDA_ABSY, LDA_INDX, LDA_INDY => {
                     self.lda(OpcodeMap.get(opcode).?.mode);
                 },
@@ -185,18 +234,21 @@ const CPU = struct {
                 INX => self.inx(),
                 else => unreachable,
             }
+
             if (initial_pc == self.pc) {
                 self.pc += OpcodeMap.get(opcode).?.len - 1;
             }
         }
     }
 
+    // fn adc(self: *CPU, mode: AddrMode) void {
+    //     const addr = self.get_op_addr(mode);
+    //     const value = self.memRead(addr);
+    // }
+
     fn lda(self: *CPU, mode: AddrMode) void {
         const addr = self.get_op_addr(mode);
         const value = self.memRead(addr);
-
-        // std.debug.print("X = 0x{x}\n", .{self.x});
-        // std.debug.print("Addr = 0x{x}\n", .{addr});
 
         self.a = value;
         self.update_zero_and_negative_flag(self.a);
@@ -257,6 +309,12 @@ const CPU = struct {
         }
     }
 };
+
+test "NOP" {
+    var cpu = CPU{};
+    cpu.loadAndRun(&[_]u8{ NOP, NOP, LDA_IMM, 0x69, BRK });
+    try expect(cpu.a == 0x69);
+}
 
 test "LDA immediate addressing" {
     var cpu = CPU{};
@@ -326,11 +384,54 @@ test "Register instructions" {
 test "Status flags" {
     var cpu = CPU{};
 
+    try expect(cpu.statusIs(.Carry) == false);
+    try expect(cpu.statusIs(.Zero) == false);
+    try expect(cpu.statusIs(.InterruptDisable) == false);
+    try expect(cpu.statusIs(.DecimalMode) == false);
+    try expect(cpu.statusIs(.Break) == false);
+    try expect(cpu.statusIs(.Break2) == false);
+    try expect(cpu.statusIs(.Overflow) == false);
+    try expect(cpu.statusIs(.Negative) == false);
+
+    cpu.statusSet(.Carry);
+    cpu.statusSet(.InterruptDisable);
+    try expect(cpu.statusIs(.Carry) == true);
+    try expect(cpu.statusIs(.Zero) == false);
+    try expect(cpu.statusIs(.InterruptDisable) == true);
+    try expect(cpu.statusIs(.DecimalMode) == false);
+    try expect(cpu.statusIs(.Break) == false);
+    try expect(cpu.statusIs(.Break2) == false);
+    try expect(cpu.statusIs(.Overflow) == false);
+    try expect(cpu.statusIs(.Negative) == false);
+
+    cpu.statusUnset(.Carry);
+    cpu.statusUnset(.InterruptDisable);
+    try expect(cpu.statusIs(.Carry) == false);
+    try expect(cpu.statusIs(.Zero) == false);
+    try expect(cpu.statusIs(.InterruptDisable) == false);
+    try expect(cpu.statusIs(.DecimalMode) == false);
+    try expect(cpu.statusIs(.Break) == false);
+    try expect(cpu.statusIs(.Break2) == false);
+    try expect(cpu.statusIs(.Overflow) == false);
+    try expect(cpu.statusIs(.Negative) == false);
+
+    cpu.statusSet(.Break);
+    cpu.statusSet(.Break2);
+    try expect(cpu.statusIs(.Carry) == false);
+    try expect(cpu.statusIs(.Zero) == false);
+    try expect(cpu.statusIs(.InterruptDisable) == false);
+    try expect(cpu.statusIs(.DecimalMode) == false);
+    try expect(cpu.statusIs(.Break) == true);
+    try expect(cpu.statusIs(.Break2) == true);
+    try expect(cpu.statusIs(.Overflow) == false);
+    try expect(cpu.statusIs(.Negative) == false);
+
     // Test zero flag
     cpu.loadAndRun(&[_]u8{ LDA_IMM, 0x00, BRK });
-    try expect(cpu.status & 0b0000_0010 == 0b0000_0010);
+    try expect(cpu.statusIs(.Zero));
+    try expect(cpu.statusIs(.Negative) == false);
 
     // Test negative flag
     cpu.loadAndRun(&[_]u8{ LDA_IMM, 0x80, BRK });
-    try expect(cpu.status & 0b1000_0000 == 0b1000_0000);
+    try expect(cpu.statusIs(.Negative));
 }
